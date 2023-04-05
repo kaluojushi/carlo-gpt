4ac507b6099fbf733627008038e4b63fb0e87371
kaluojushi <694802801@qq.com>
2023-04-05 16:28:29 +0800

    修复vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,74 +4,51 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig((env) => {
-  const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
-
-  return {
-    resolve: {
-      alias: {
-        '@': path.resolve(process.cwd(), 'src'),
-      },
-    },
-    plugins: [
-      vue(),
-      VitePWA({
-        injectRegister: 'auto',
-        manifest: {
-          name: 'CarloGPT',
-          short_name: 'CarloGPT',
-          icons: [
-            { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-            { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-          ],
-        },
-      }),
-    ],
 function setupPlugins(env: ImportMetaEnv): PluginOption[] {
-  return [
-    vue(),
-    env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
-      injectRegister: 'auto',
-      manifest: {
-        name: 'chatGPT',
-        short_name: 'chatGPT',
-        icons: [
-          { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
-        ],
-      },
-    }),
-  ]
+	return [
+		vue(),
+		env.VITE_GLOB_APP_PWA === 'true' && VitePWA({
+			injectRegister: 'auto',
+			manifest: {
+				name: 'CarloGPT',
+				short_name: 'CarloGPT',
+				icons: [
+					{ src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+					{ src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
+				],
+			},
+		}),
+	]
 }
 
 export default defineConfig((env) => {
-  const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
+	const viteEnv = loadEnv(env.mode, process.cwd()) as unknown as ImportMetaEnv
 
-  return {
-    resolve: {
-      alias: {
-        '@': path.resolve(process.cwd(), 'src'),
-      },
-    },
-    plugins: setupPlugins(viteEnv),
-    server: {
-      host: '0.0.0.0',
-      port: 1002,
-      open: false,
-      proxy: {
-        '/api': {
-          target: viteEnv.VITE_APP_API_BASE_URL,
-          changeOrigin: true, // 允许跨域
-          rewrite: path => path.replace('/api/', '/'),
-        },
-      },
-    },
-    build: {
-      reportCompressedSize: false,
-      sourcemap: false,
-      commonjsOptions: {
-        ignoreTryCatch: false,
-      },
-    },
-  }
+	return {
+		resolve: {
+			alias: {
+				'@': path.resolve(process.cwd(), 'src'),
+			},
+		},
+		plugins: setupPlugins(viteEnv),
+		server: {
+			host: '0.0.0.0',
+			port: 1002,
+			open: false,
+			proxy: {
+				'/api': {
+					target: viteEnv.VITE_APP_API_BASE_URL,
+					changeOrigin: true, // 允许跨域
+					rewrite: path => path.replace('/api/', '/'),
+				},
+			},
+		},
+		build: {
+			reportCompressedSize: false,
+			sourcemap: false,
+			commonjsOptions: {
+				ignoreTryCatch: false,
+			},
+		},
+	}
 })
